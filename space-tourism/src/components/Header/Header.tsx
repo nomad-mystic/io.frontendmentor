2b6250707b1635a3f4af82023d7a18acffecb35a
nomad-mystic <nomadmystics@gmail.com>
2023-11-08 11:16:09 -0800

    kpm: Add tailwindcss/nesting to the project

--- space-tourism/src/components/Header/Header.tsx
+++ space-tourism/src/components/Header/Header.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+// Styles
+import './Header.css';
+
 // Compontents
 import HeaderNav from '../HeaderNav/HeaderNav.tsx';
 
 const Header = (): React.JSX.Element => {
     return (
         <header className="Header">
-
+            <h1 className="Header-test">Testing</h1>
             <HeaderNav />
         </header>
     );
--- space-tourism/src/components/HeaderNav/HeaderNav.tsx
+++ space-tourism/src/components/HeaderNav/HeaderNav.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// Styles
+import './HeaderNav.css';
+
 const HeaderNav = (): React.JSX.Element => {
     return (
         <nav>
