f6bae2a4f930b8ee1d0d847846f55ef36bc26e3f
nomad-mystic <nomadmystics@gmail.com>
2023-11-08 11:24:30 -0800

    kpm: Start the process of building the Header

--- space-tourism/src/components/Header/Header.tsx
+++ space-tourism/src/components/Header/Header.tsx
@@ -9,7 +9,6 @@ import HeaderNav from '../HeaderNav/HeaderNav.tsx';
 const Header = (): React.JSX.Element => {
     return (
         <header className="Header">
-            <h1 className="Header-test">Testing</h1>
             <HeaderNav />
         </header>
     );
--- space-tourism/src/components/HeaderNav/HeaderNav.tsx
+++ space-tourism/src/components/HeaderNav/HeaderNav.tsx
@@ -5,8 +5,13 @@ import './HeaderNav.css';
 
 const HeaderNav = (): React.JSX.Element => {
     return (
-        <nav>
-
+        <nav className="HeaderNav">
+            <ul className="flex">
+                <li className="NavText">00 Home</li>
+                <li className="NavText">01 Destination</li>
+                <li className="NavText">02 Crew</li>
+                <li className="NavText">03 Technology</li>
+            </ul>
         </nav>
     );
 };
