e6b8838bd10b670ed08aa9ff4fc129cd1961715a
nomad-mystic <nomadmystics@gmail.com>
2023-11-13 12:22:59 -0800

    kpm: Continue the process of building the crew page

--- space-tourism/src/pages/crew/CrewPage.tsx
+++ space-tourism/src/pages/crew/CrewPage.tsx
@@ -3,10 +3,7 @@ import React, { ReactNode, useState } from 'react';
 // Styles
 import './CrewPage.css';
 import Header from '../../components/Header/Header.tsx';
-import data from '../destination/data/data.json';
-import DestinationDataInterface from '../destination/interfaces/DestinationDataInterface.ts';
-import DestinationImage from '../destination/components/DestinationImage.tsx';
-import DestinationContent from '../destination/components/DestinationContent.tsx';
+import data from '../crew/data/data.json';
 import ElementUtils from '../../utils/ElementUtils.ts';
 
 /**
@@ -17,7 +14,7 @@ import ElementUtils from '../../utils/ElementUtils.ts';
  * @return {React.JSX.Element}
  */
 const CrewPage = (): React.JSX.Element => {
-    const [activeCrewMember, setActiveCrewMember] = useState('moon');
+    const [activeCrewMember, setActiveCrewMember] = useState(1);
 
     /**
      * @description
@@ -33,12 +30,12 @@ const CrewPage = (): React.JSX.Element => {
         // Sanity check
         if (target && typeof target !== 'undefined' && target.tagName === 'LI') {
 
-            // Reset our active class
-            ElementUtils.removeClassFromAllElements(window.document.querySelectorAll('.DestinationPage-main .DestinationPage-nav menu li'));
-            target.classList.add('active');
-
-            // Update the state
-            setActiveCrewMember(target.id);
+            // // Reset our active class
+            // ElementUtils.removeClassFromAllElements(window.document.querySelectorAll('.DestinationPage-main .DestinationPage-nav menu li'));
+            // target.classList.add('active');
+            //
+            // // Update the state
+            // setActiveCrewMember(target.id);
         }
     };
 
@@ -46,39 +43,31 @@ const CrewPage = (): React.JSX.Element => {
         <>
             <Header/>
 
-            <main className="DestinationPage-main max-w-container">
-                <section className="DestinationPage-left">
-                    <h2 className="DestinationPage-title Heading-5 uppercase text-white">
-                        <span className="DestinationPage-number">02</span> Meet your crew</h2>
+            <main className="CrewPage-main max-w-container">
+                <section className="CrewPage-left">
+                    <h2 className="CrewPage-heading Heading-5 uppercase text-white">{/*@todo Change this in the DestinationPage */}
+                        <span className="CrewPage-number">02</span> Meet your crew</h2>
 
-                    {
-                        data.map((item: DestinationDataInterface): ReactNode => {
-                            if (activeCrewMember === item.id) {
-                                return (<DestinationImage key={ item.id } data={ item }/>)
-                            }
-                        })
-                    }
+                    <article className="CrewPage-content">
+                        <h2 className="CrewPage-title Heading-4">Commander</h2>
+                        <h1 className="CrewPage-name Heading-3">Douglas Hurley</h1>
+                        <p className="CrewPage-text Subheading-2 max-w-[444px] block">Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
+                    </article>
 
-                </section>
-
-                <section className="DestinationPage-right">
-                    <nav className="DestinationPage-nav">
+                    <nav className="CrewPage-nav">
                         <menu className="flex gap-[36px]" onClick={ toggleActive }>
-                            <li className="DestinationPage-navItem NavText active" id="moon">MOON</li>
-                            <li className="DestinationPage-navItem NavText" id="mars">MARS</li>
-                            <li className="DestinationPage-navItem NavText" id="europa">EUROPA</li>
-                            <li className="DestinationPage-navItem NavText" id="titan">TITAN</li>
+                            <li className="CrewPage-navItem NavText active" id="douglas"></li>
+                            <li className="CrewPage-navItem NavText" id="2"></li>
+                            <li className="CrewPage-navItem NavText" id="3"></li>
+                            <li className="CrewPage-navItem NavText" id="4"></li>
                         </menu>
                     </nav>
+                </section>
 
-                    {
-                        data.map((item: DestinationDataInterface): ReactNode => {
-                            if (activeCrewMember === item.id) {
-                                return (<DestinationContent key={ item.id } data={ item }/>)
-                            }
-                        })
-                    }
-
+                <section className="CrewPage-right">
+                    <figure>
+                        <img src="/crew/image-douglas-hurley.webp" alt=""/>
+                    </figure>
                 </section>
             </main>
         </>
