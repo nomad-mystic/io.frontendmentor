67736f715b72bffa3003c2014e022453487d90f1
nomad-mystic <nomadmystics@gmail.com>
2023-12-20 00:25:58 -0800

    kpm: Continue the process of building the mobile styles

--- space-tourism/src/components/HeaderNav/HeaderNav.tsx
+++ space-tourism/src/components/HeaderNav/HeaderNav.tsx
@@ -14,6 +14,13 @@ import './HeaderNav.css';
 const HeaderNav = (): React.JSX.Element => {
     return (
         <nav className="HeaderNav max-w-[830px] w-full">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 20 21" fill="none" className="HeaderNav-close">
+                <rect x="2.5752" y="0.954102" width="24" height="3" transform="rotate(45 2.5752 0.954102)"
+                      fill="#D0D6F9" />
+                <rect x="0.454102" y="17.9246" width="24" height="3" transform="rotate(-45 0.454102 17.9246)"
+                      fill="#D0D6F9" />
+            </svg>
+
             <ul className="flex flex-col  md:flex-row md:gap-[50px]">
                 <li className="NavText">
                     <NavLink to="/">
