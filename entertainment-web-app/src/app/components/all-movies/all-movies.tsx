07efbe841811bd77fe885e7a4c93e650229adcf7
nomad-mystic <nomadmystics@gmail.com>
2024-01-24 17:41:45 -0800

    kpm: Stub out components

---
+++ entertainment-web-app/src/app/components/all-movies/all-movies.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllMovies = () => {
+    return (
+        <section className="AllMovies">
+            <h2 className="header-l">Recommended for you</h2>
+        </section>
+    );
+};
+
+export default AllMovies;
